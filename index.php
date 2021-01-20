33e217147642591fdaf7c0c741112995badcfed5
MehmetKandamar <72360607+MehmetKandamar@users.noreply.github.com>
2021-01-20 11:29:58 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,36 @@
+<?php
+
+// database bağlantısı
+$db = mysqli_connect("localhost", "root", "", "feeds");
+
+if (isset($_POST['submit'])){
+    $newUrl = $db->real_escape_string($_POST['newUrl']);
+    $sql = "INSERT INTO urls (url) VALUE ('$newUrl')";
+    mysqli_query( $db, $sql);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title>RSS FEED</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<title>RSS FEED</title>
+<link rel="stylesheet" href="style.css">
  <body>
   <?php
 
      function rssRead($url){
-
+         $db= mysqli_connect("localhost", "root", "", "feeds");
        $take = simplexml_load_file($url);
+         $title=  $take->channel->title
+?>
+         <h2 id="tit" ><?php echo "$title" ?></h2>
 
+         <?php
        echo "<ul>";
 
        foreach ($take->channel->item as $entry){?>
        <div class="relative">
            <?php
-           $db= mysqli_connect("localhost", "root", "", "feeds");
            $date = $take->channel->lastBuildDate;
                 echo "<h4><li><a href ='$entry->link' title='entry->title' target='_blank'>". $entry->title."</a></li></h4>";
                 echo $entry->description."<br><br><br><br><br><br><br>" ;
@@ -46,28 +60,22 @@
    ?>
   </div>
 
-  <h2 id="tit" >Ekonomi Haberleri</h2>
   <?php
-
-  echo rssRead("http://www.trt.net.tr/rss/ekonomi.rss") ;
-  echo "<br>";
-
+  $sqll = 'SELECT * FROM urls';
+  $getData = mysqli_query($db,$sqll);
+  foreach ($getData as $sqlurl)
+  {
+      rssRead($sqlurl['url']);
+      echo "<br>";
+  }
   ?>
 
-  <h2 id="tit" >Dünyadan Haberler</h2>
-
-  <?php
-
-  echo rssRead( "http://www.trt.net.tr/rss/dunya.rss");
-  echo "<br>";
- ?>
-  <h2 id="tit" >Spor Haberleri</h2>
-
-  <?php
-
-  echo rssRead( "http://www.trt.net.tr/rss/spor.rss");
-  echo "<br>";
-  ?>
+  <form method="post" action=" " class="input_form">
+      <label>
+          <input type="text" name="newUrl" class="newUrl">
+      </label>
+      <button type="submit" name="submit" id="add_btn" class="add_btn">Add Url</button>
+  </form>
 
 
  </body>
